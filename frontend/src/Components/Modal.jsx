661e2d5e88e7062f8350bc965e9aa5952d59d62d
Parker <xparkerland@gmail.com>
2024-10-06 00:07:20 +1000

    The real fix to indentation

--- frontend/src/Components/Modal.jsx
+++ frontend/src/Components/Modal.jsx
@@ -5,28 +5,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Modal({ closeModal }) {
-	return (
-		<>
-			<div className='modal-background'>
-				<div className='modal-container'>
-					<div className='modal-close-btn'>
-						<button onClick={() => closeModal(false)}>
-							<FontAwesomeIcon className='close-btn' icon={faXmark} />
-						</button>
-					</div>
-					<div className='title'>
-						<p>Please scan the mother's barcode</p>
-					</div>
-					<div className='body'>
-						<input type="text" className="scanner-input" disabled></input>
-						<img src={scanner} alt="scanner" />
-					</div>
-					<div className='footer'>
-						waiting for scan...
-					</div>
-				</div>
-			</div>
-		</>
-	)};
+  return (
+    <>
+      <div className='modal-background'>
+        <div className='modal-container'>
+          <div className='modal-close-btn'>
+            <button onClick={() => closeModal(false)}>
+              <FontAwesomeIcon className='close-btn' icon={faXmark} />
+            </button>
+          </div>
+          <div className='title'>
+            <p>Please scan the mother's barcode</p>
+          </div>
+          <div className='body'>
+            <input type="text" className="scanner-input" disabled></input>
+            <img src={scanner} alt="scanner" />
+          </div>
+          <div className='footer'>
+            waiting for scan...
+          </div>
+        </div>
+      </div>
+    </>
+  )};
 
 export default Modal
