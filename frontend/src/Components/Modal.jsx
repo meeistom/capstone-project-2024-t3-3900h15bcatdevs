208325385276699874a2767b55a1128446893e79
Parker <xparkerland@gmail.com>
2024-10-07 18:57:44 +1100

    Implemented form for milk detail

--- frontend/src/Components/Modal.jsx
+++ frontend/src/Components/Modal.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import "../index.css";
 import scanner from "../Assets/scanner.png";
+import { Form } from "./Form.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
@@ -9,7 +10,11 @@ export { Modal };
 function Modal({ closeModal, version }) {
   const scannerInputRef = useRef(null);
   const [modalVersion, setModalVersion] = useState(version);
+  const [scannedValue, setScannedValue] = useState(0);
+  const [expiryDate, setExpiryDate] = useState("");
+  const [expressDate, setExpressDate] = useState("")
   let title, body, footer;
+
   useEffect(() => {
     if (scannerInputRef.current) {
       scannerInputRef.current.focus();
@@ -18,10 +23,20 @@ function Modal({ closeModal, version }) {
   }, [])
   
   const handleInput = () => {
-    const scannedValue = scannerInputRef.current.value;
+    setScannedValue(scannerInputRef.current.value);
     // console.log(scannedValue); // for debug
     if (scannedValue.length >= 8) { // this will be changed later on to data matching
       setModalVersion("addMilk2");
+    }
+  }
+
+  const handleSubmitMilkInfo = () => {
+    if (!expiryDate || !expressDate) {
+      console.log(expiryDate)
+      console.log(expressDate)
+      alert("Please fill in all relevant infomations^ ^");
+    } else {
+      setModalVersion("addMilk3");
     }
   }
 
@@ -31,16 +46,33 @@ function Modal({ closeModal, version }) {
       title = "Please scan the mother's barcode";
       body = (
         <>
-        <input type="text" className="scanner-input" ref={scannerInputRef} onChange={handleInput}></input>
-        <img src={scanner} alt="scanner" />
+          <input type="text" className="scanner-input" ref={scannerInputRef} onChange={handleInput}></input>
+          <img src={scanner} alt="scanner" />
         </>
       );
       footer = "Waiting for scan...";
       break;
     case "addMilk2":
       title = "Confirm Infomation";
-      body = "this will be a form"
-      footer = "and this will be two btns"
+      body = (
+        <>
+          <Form mrn={scannedValue} expiryDate={expiryDate} expressDate={expressDate} setExpressDate={setExpressDate} setExpiryDate={setExpiryDate}/>
+        </>
+      )
+      footer = (
+        <>
+         <button onClick={() => closeModal(false)} type="button" class="btn btn-outline-primary">Cancel</button>
+         <button type="button" class="btn btn-primary" onClick={handleSubmitMilkInfo}>Confirm</button>
+        </>
+      )
+      break;
+    case "addMilk3":
+      title = "sticker preview";
+      body = "pretend im a picture";
+      footer = "pretend im two btns";
+      break;
+    case "addMilk4":
+      // prepared for merge conflict
   }
   return (
     <>
