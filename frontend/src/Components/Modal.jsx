c94e6e45dad3003c4c947702644f6edfdc1c4559
skyuanow <skyuanow@gmail.com>
2024-10-07 17:27:42 +1100

    made confirmation section for Add Milk Modal

--- frontend/src/Components/Modal.jsx
+++ frontend/src/Components/Modal.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import "../index.css";
 import scanner from "../Assets/scanner.png";
+import confirmCheck from "../Assets/confirm-check.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
 
 export { Modal };
 
@@ -15,15 +17,16 @@ function Modal({ closeModal, version }) {
       scannerInputRef.current.focus();
       // console.log("Input field focused"); // for debug
     }
-  }, [])
-  
+  }, []);
+
   const handleInput = () => {
     const scannedValue = scannerInputRef.current.value;
     // console.log(scannedValue); // for debug
-    if (scannedValue.length >= 8) { // this will be changed later on to data matching
+    if (scannedValue.length >= 8) {
+      // this will be changed later on to data matching
       setModalVersion("addMilk2");
     }
-  }
+  };
 
   // handle which version of modal is rendered
   switch (modalVersion) {
@@ -31,36 +34,58 @@ function Modal({ closeModal, version }) {
       title = "Please scan the mother's barcode";
       body = (
         <>
-        <input type="text" className="scanner-input" ref={scannerInputRef} onChange={handleInput}></input>
-        <img src={scanner} alt="scanner" />
+          <input
+            type="text"
+            className="scanner-input"
+            ref={scannerInputRef}
+            onChange={handleInput}
+          ></input>
+          <img src={scanner} alt="scanner" />
         </>
       );
       footer = "Waiting for scan...";
       break;
     case "addMilk2":
       title = "Confirm Infomation";
-      body = "this will be a form"
-      footer = "and this will be two btns"
+      body = "this will be a form";
+      footer = (
+        <>
+          <div>and this will be two btns</div>
+          <Button onClick={() => setModalVersion("addMilkComplete")}>
+            Next
+          </Button>
+        </>
+      );
+      break;
+    case "addMilkComplete":
+      title = "Added Milk Successfully";
+      body = (
+        <>
+          <div>
+            Milk from the Mother was added. You may close the pop up now.
+          </div>
+          <img src={confirmCheck} alt="Confirmation Icon" />
+        </>
+      );
+      footer = <Button onClick={() => closeModal(false)}>Return Home</Button>;
+      break;
   }
   return (
     <>
-      <div className='modal-background'>
-        <div className='modal-container'>
-          <div className='modal-close-btn'>
+      <div className="modal-background">
+        <div className="modal-container">
+          <div className="modal-close-btn">
             <button onClick={() => closeModal(false)}>
-              <FontAwesomeIcon className='close-btn' icon={faXmark} />
+              <FontAwesomeIcon className="close-btn" icon={faXmark} />
             </button>
           </div>
-          <div className='title'>
+          <div className="title">
             <p>{title}</p>
           </div>
-          <div className='body'>
-            {body}
-          </div>
-          <div className='footer'>
-            {footer}
-          </div>
+          <div className="body">{body}</div>
+          <div className="footer">{footer}</div>
         </div>
       </div>
     </>
-  )};
+  );
+}
