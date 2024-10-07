4a39fb9689484d2aaa8645dce69d5130967dab6d
Gabriel Thien <53280648+losgab@users.noreply.github.com>
2024-10-08 02:46:01 +1100

    added mother and nav buttons for registration

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -10,11 +10,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquare, faCheckSquare } from "@fortawesome/free-regular-svg-icons";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 export { Register };
 
 function Register() {
-  const [currentRenderedPage, setCurrentRenderedPage] = useState(0);
+  let [currentRenderedPage, setCurrentRenderedPage] = useState(0);
+  const [pageIndex, setPageIndex] = useState(0);
   // Page 0: Choose Mother/Baby/Milk to register
   // Page 1: Mother Registration
   // Page 2: Baby Registration
@@ -23,37 +26,37 @@ function Register() {
   // Page 5: Preview generated label
   // Page 6: Print label
   
-  var pagesToVisit = [0];
-  var currentPage = 0;
+  let pagesToVisit = [0];
+
   const nextPageToVisit = () => {
-    currentPage += 1;
-    console.log("Next Page: " + pagesToVisit[currentPage]);
-    setCurrentRenderedPage(pagesToVisit[currentPage]);
+    setCurrentRenderedPage(pagesToVisit[pageIndex + 1]);
+    setPageIndex(pageIndex => pageIndex + 1);
   };
   
   const prevPageToVisit = () => {
-    currentPage -= 1;
-    console.log("Prev Page: " + pagesToVisit[currentPage]);
-    setCurrentRenderedPage(pagesToVisit[currentPage]);
+    if (pageIndex - 1 >= 0) {
+      setCurrentRenderedPage(pagesToVisit[pageIndex - 1]);
+      setPageIndex(pageIndex => pageIndex - 1);
+    }
   };
   
   // Registration Type - Mother, Baby, Milk
   const [checked, setChecked] = useState([false, false, false]);
   
   const renderPage = () => {
-    console.log("Current Page: " + pagesToVisit[currentPage]);
+    console.log("Page Index: " + pageIndex);
     switch (currentRenderedPage) {
       case 0:
-        return <ChooseRegistrationTypePage
+        return <ChooseRegistrationType
         checked={checked}
         setChecked={setChecked}
         />
       case 1:
-        return <MotherRegistrationPage />;
+        return <MotherRegistration />;
       case 2:
-        return <BabyRegistrationPage />;
+        return <BabyRegistration />;
       case 3:
-        return <MilkRegistrationPage />;
+        return <MilkRegistration />;
     }
   }
           
@@ -61,59 +64,53 @@ function Register() {
     <>
       <section id="Home">
         <Navibar />
-        <div className="register-title">
-          <h1>{"Register"}</h1>
-        </div>
-        {renderPage()}
-        <div className='nav-button-container'>
-          <div className='back-button'>
-            {currentRenderedPage > 0 && (
-              <Button 
+        <div className="register-container">
+          <div className="register-title">
+            <h1>{"Register"}</h1>
+          </div>
+          {renderPage()}
+          <div className='nav-button-container'>
+            <div className="back-button-container">
+              {currentRenderedPage > 0 && (
+                <Button
                 variant="primary"
                 size="lg"
                 onClick={() => {
                   prevPageToVisit();
                 }}
                 >
-                <FontAwesomeIcon icon={faArrowLeft} />
-                <span style={{ marginLeft: '10px' }}>Back</span>
+                  <FontAwesomeIcon icon={faArrowLeft} />
+                  <span style={{ marginLeft: '10px' }}>Back</span>
+                </Button>
+              )}  
+            </div>
+            <div className="next-button-container">
+              <Button 
+                variant="primary"
+                size="lg"
+                onClick={() => {
+                  const hasTrue = checked.some(element => element === true); // Checks for a strict `true`
+                  if (!hasTrue) {
+                    alert("Choose one type for registration");
+                    return;
+                  }
+
+                  if (currentRenderedPage == 0) {
+                    checked.forEach((type, index) => { // Add pages to visit based on what was checked
+                      if (!pagesToVisit.includes(index + 1) && type) {
+                        pagesToVisit.push(index + 1);
+                      }
+                    });
+                  }
+
+                  console.log(pagesToVisit);
+                  nextPageToVisit();
+                }}
+                >
+                <span style={{ marginRight: '10px' }}>Next</span>
+                <FontAwesomeIcon icon={faArrowRight} />
               </Button>
-            )}  
-          </div>
-          <div className='next-button'>
-            <Button 
-              variant="primary"
-              size="lg"
-              onClick={() => {
-                if (currentPage == 0) {
-                  checked.forEach((type, index) => { // Add pages to visit based on what was checked
-                    if (type) {
-                      pagesToVisit.push(index + 1);
-                    }
-                  });
-                }
-                nextPageToVisit();
-                console.log(pagesToVisit);
-                // if (checked[0] && !visited[0]) { // Move to Mother Registration Page
-                //   setCurrentPage(1);
-                //   visited[0] = true;
-                //   console.log("Moving to Mother Registration Page");
-                // } else if (checked[1] && !visited[1]) { // Move to Baby Registration Page
-                //   setCurrentPage(2);
-                //   visited[1] = true; 
-                //   console.log("Moving to Baby Registration Page");
-                // } else if (checked[2] && !visited[2]) { // Move to Milk Registration Page
-                //   setCurrentPage(3);
-                //   visited[2] = true;
-                //   console.log("Moving to Milk Registration Page");
-                // } else {
-                //   alert("Please select at least one option.");
-                // }
-              }}
-              >
-              <span style={{ marginRight: '10px' }}>Next</span>
-              <FontAwesomeIcon icon={faArrowRight} />
-            </Button>
+            </div>  
           </div>
         </div>
       </section>
@@ -121,7 +118,7 @@ function Register() {
   );
 }
 
-const ChooseRegistrationTypePage = ({checked, setChecked}) => {
+const ChooseRegistrationType = ({checked, setChecked}) => {
   const handleToggle = (index) => {
     const newChecked = [...checked];
     newChecked[index] = !newChecked[index];
@@ -186,18 +183,23 @@ const ChooseRegistrationTypePage = ({checked, setChecked}) => {
   )
 }
 
-const MotherRegistrationPage = () => (
-  <div className="register-selection-container">
-    {/* Render the progress bar at mother details stage */}
-    <div className='title'>
-      <h2>{"Mother Details"}</h2>
-    </div>
-
+const MotherRegistration = () => (
+  <div className="mother-details-container">
+    <h1>{"Mother Details"}</h1>
+    <div className='mrn'>MRN</div>
+    <Form.Control type="email" placeholder="Enter MRN" />
+    <Form.Text className="text-muted"></Form.Text>  
+    <div className='mrn'>First Name</div>
+    <Form.Control type="email" placeholder="Enter First Name" />
+    <Form.Text className="text-muted"></Form.Text>  
+    <div className='mrn'>Last Name</div>
+    <Form.Control type="email" placeholder="Enter Last Name" />
+    <Form.Text className="text-muted"></Form.Text>  
   </div>
 );
 
-const BabyRegistrationPage = () => (
-  <div className="register-selection-container">
+const BabyRegistration = () => (
+  <div className="input-details-container">
     {/* Render the progress bar at mother details stage */}
     <div className='title'>
       <h2>{"Baby Details"}</h2>
@@ -205,8 +207,8 @@ const BabyRegistrationPage = () => (
   </div>
 );
            
-const MilkRegistrationPage = () => (
-  <div className="register-selection-container">
+const MilkRegistration = () => (
+  <div className="input-details-container">
     {/* Render the progress bar at mother details stage */}
     <div className='title'>
       <h2>{"Milk Details"}</h2>
@@ -214,7 +216,7 @@ const MilkRegistrationPage = () => (
   </div>
 );
            
-const ConfirmDetailsPage = () => (
+const ConfirmDetails = () => (
   <div className="register-selection-container">
     {/* Render the progress bar at mother details stage */}
     <div className='title'>
@@ -223,7 +225,7 @@ const ConfirmDetailsPage = () => (
   </div>
 );
 
-const PreviewGeneratedLabelPage = () => (
+const PreviewGeneratedLabel = () => (
   <div className="register-selection-container">
     {/* Render the progress bar at mother details stage */}
     <div className='title'>
@@ -232,7 +234,7 @@ const PreviewGeneratedLabelPage = () => (
   </div>
 );  
 
-const PrintLabelPage = () => (
+const PrintLabel = () => (
   <div className="register-selection-container">
     {/* Render the progress bar at mother details stage */}
     <div className='title'>
