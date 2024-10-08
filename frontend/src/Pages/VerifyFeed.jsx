46d420ebb8ed10e323af721b1f0ec8da9e19b206
Cynthia <cynthia.li5456@gmail.com>
2024-10-09 02:03:34 +1100

    Verification success case frontend complete

--- frontend/src/Pages/VerifyFeed.jsx
+++ frontend/src/Pages/VerifyFeed.jsx
@@ -1,30 +1,95 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navibar } from "../Components/Navibar";
 import scanner from "../Assets/scanner.png";
+import confirmCheck from "../Assets/confirm-check.png"
 
 export { VerifyFeed };
 
 function VerifyFeed() {
+  const scannerInputRef = useRef(null);
+  const [milkCheck, setMilkCheck] = useState(<span className={"empty-check"}></span>);
+  const [babyCheck, setBabyCheck] = useState(<span className={"empty-check"}></span>);
+  const [promptMessage, setPromptMessage] = useState("Please scan the barcode on the baby or milk.");
+  const [promptType, setPromptType] = useState("verificationPrompt");
+  let promptPage;
+
+  useEffect(() => {
+    if (scannerInputRef.current) {
+      scannerInputRef.current.focus();
+    }
+  }, []);
+
+  const handleInput = () => {
+    const scannedValue = scannerInputRef.current.value;
+    if (scannedValue.length >= 0) {
+      if (scannedValue === "milk") {
+        // Will need to check expiry
+        setMilkCheck(<img className="img" src={confirmCheck}></img>)
+        if (babyCheck.type === 'img') {
+          // Will need to check for match
+          setPromptType("confirmation")
+        } else {
+          setPromptMessage("Please scan the barcode on the baby.")
+        }
+      } else if (scannedValue === "baby") {
+        setBabyCheck(<img className="img" src={confirmCheck}></img>)
+        if (milkCheck.type === 'img') {
+          // Will need to check for match
+          setPromptType("confirmation")
+        } else {
+          setPromptMessage("Please scan the barcode on the milk.")
+        }
+      }
+    }
+  };
+
+  switch (promptType) {
+    case "verificationPrompt":
+      promptPage = (
+        <>
+          <div className="subtitle">{promptMessage}</div>
+          <img className="img" src={scanner}></img>
+          <div className="text">Waiting for scan...</div>
+          <input
+            type="text"
+            className="scanner-input"
+            ref={scannerInputRef}
+            onChange={handleInput}
+          ></input>
+        </>
+      );
+      break;
+    case "confirmation":
+      promptPage = (
+        <>
+        <div>
+          <div className="subtitle">Verification Complete!</div>
+          <div className="text" style={{marginTop: "10px"}}>Feed has been recorded.</div>
+        </div>
+        <button type="button" class="btn btn-primary home-button">Return Home</button>
+        </>
+      )
+      break;
+  };
+
   return (
     <>
       <section id="Home">
         <Navibar />
-        <div className="verify-container">
+        <div className="verification-page">
           <div className="title">Verify Feed</div>
           <div className="checkbox-container">
             <div className="checkbox">
-              <span className="empty-check"></span>
+              <div>{babyCheck}</div>
               <div className="text">Baby</div>
             </div>
             <div className="checkbox">
-              <span className="empty-check"></span>
+              <div>{milkCheck}</div>
               <div className="text">Bottle</div>
             </div>
           </div>
-          <div className="subtitle"> Please scan the barcode on the baby or the milk.</div>
-          <img className="img" src={scanner}></img>
-          <div className="text">Waiting for scan...</div>
+          <div className="prompt-container">{promptPage}</div>
         </div>
     </section>
     </>
