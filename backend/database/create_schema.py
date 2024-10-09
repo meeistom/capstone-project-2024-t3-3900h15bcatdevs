821cdec4aa4729957de16fa662215176232067d6
Aolin Xu <aolinzamxu@gmail.com>
2024-10-09 20:38:15 +1100

    added create bottle table

--- backend/database/create_schema.py
+++ backend/database/create_schema.py
@@ -60,6 +60,40 @@ def create_baby_table():
     except psycopg2.Error as e:
         print(f"An error occurred while creating the table: {e}")
 
+def create_bottle_table():
+    try:
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(
+            dbname="milk_checker",
+            user="username",
+            password="password",
+            host="localhost",
+            port=5432
+        )
+        cur = conn.cursor()
+        # Create the table if it doesn't already exist
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS bottle (
+                id SERIAL PRIMARY KEY,
+                milk_type VARCHAR(50),
+                bottle_quantity INT,
+                express_time TIMESTAMP,
+                storage_method VARCHAR(50),
+                storage_location VARCHAR(50),
+                extra_notes VARCHAR(50),
+                barcode VARCHAR(50)
+            );
+        ''')
+        conn.commit()
+        print("Table created successfully.")
+
+        # Close the cursor and connection
+        cur.close()
+        conn.close()
+    except psycopg2.Error as e:
+        print(f"An error occurred while creating the table: {e}")
+
 if __name__ == "__main__":
+    create_bottle_table()
     create_mother_table()
     create_baby_table()
