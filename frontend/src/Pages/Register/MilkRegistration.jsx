c99e603b6f12b3c822b8e767e563aa53d1d6f10c
skyuanow <skyuanow@gmail.com>
2024-10-22 19:58:37 +1100

    made MilkRegistration use Form component

--- frontend/src/Pages/Register/MilkRegistration.jsx
+++ frontend/src/Pages/Register/MilkRegistration.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../index.css";
 
@@ -22,84 +23,95 @@ function MilkRegistration({
         <div className="title">
           <h2>{"Milk Details"}</h2>
         </div>
-        <div className="form-milk-detail" class="mb-3">
-          <div class="container">
-            <div class="row row-cols-2">
-              <div class="col">
-                <label htmlFor="milk-type" class="register-form-label">
-                  Milk Type*
-                </label>
-                <select
-                  id="milk-type"
-                  className="form-select form-select-sm"
-                  value={milkType}
-                  onChange={(e) => setMilkType(e.target.value)}
-                >
-                  <option value="ehm">EHM</option>
-                  <option value="pdhm">PDHM</option>
-                  <option value="humavant6">Humavant 6</option>
-                  <option value="Cream">Cream</option>
-                  <option value="HMF">HMF</option>
-                </select>
-              </div>
-              <div class="col">
-                <label htmlFor="milk-storage" class="register-form-label">
-                  Storage Type*
-                </label>
-                <select
-                  id="milk-storage"
-                  class="form-select form-select-sm"
-                  value={storageType}
-                  onChange={(e) => setStorageType(e.target.value)}
-                >
-                  <option value="fridge">Fridge</option>
-                  <option value="fresh">Fresh</option>
-                  <option value="defrost">Defrost</option>
-                </select>
-              </div>
-              <div class="col">
-                <label htmlFor="express-date" class="register-form-label">
-                  Expressed Date*
-                </label>
-                <input
-                  value={expressDate}
-                  onChange={(e) => {
-                    setExpressDate(e.target.value);
-                  }}
-                  class="form-control"
-                  id="express-date"
-                  type="datetime-local"
-                />
-              </div>
-              <div class="col">
-                <label htmlFor="expiry-date" class="register-form-label">
-                  Expiry Date*
-                </label>
-                <input
-                  value={expiryDate}
-                  onChange={(e) => {
-                    setExpiryDate(e.target.value);
-                  }}
-                  class="form-control"
-                  id="expiry-date"
-                  type="datetime-local"
-                />
+        <Form>
+          <div className="form-milk-detail" class="mb-3">
+            <div class="container">
+              <div class="row row-cols-2">
+                <div class="col">
+                  <Form.Label htmlFor="milk-type" className="form-label">
+                    Milk Type*
+                  </Form.Label>
+                  <Form.Select
+                    id="milk-type"
+                    className="form-select form-select-sm"
+                    value={milkType}
+                    onChange={(e) => setMilkType(e.target.value)}
+                  >
+                    <option value="ehm">EHM</option>
+                    <option value="pdhm">PDHM</option>
+                    <option value="humavant6">Humavant 6</option>
+                    <option value="Cream">Cream</option>
+                    <option value="HMF">HMF</option>
+                  </Form.Select>
+                </div>
+                <div class="col">
+                  <Form.Label
+                    htmlFor="milk-storage"
+                    className="register-form-label"
+                  >
+                    Storage Type*
+                  </Form.Label>
+                  <Form.Select
+                    id="milk-storage"
+                    className="form-select form-select-sm"
+                    value={storageType}
+                    onChange={(e) => setStorageType(e.target.value)}
+                  >
+                    <option value="fridge">Fridge</option>
+                    <option value="fresh">Fresh</option>
+                    <option value="defrost">Defrost</option>
+                  </Form.Select>
+                </div>
+                <div class="col">
+                  <Form.Label
+                    htmlFor="express-date"
+                    className="register-form-label"
+                  >
+                    Expressed Date*
+                  </Form.Label>
+                  <input
+                    value={expressDate}
+                    onChange={(e) => {
+                      setExpressDate(e.target.value);
+                    }}
+                    class="form-control"
+                    id="express-date"
+                    type="datetime-local"
+                  />
+                </div>
+                <div class="col">
+                  <Form.Label
+                    htmlFor="expiry-date"
+                    className="register-form-label"
+                  >
+                    Expiry Date*
+                  </Form.Label>
+                  <input
+                    value={expiryDate}
+                    onChange={(e) => {
+                      setExpiryDate(e.target.value);
+                    }}
+                    class="form-control"
+                    id="expiry-date"
+                    type="datetime-local"
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="mb-3">
-          <label htmlFor="milk-notes" class="register-form-label">
-            Notes
-          </label>
-          <textarea
-            class="form-control"
-            id="milk-notes"
-            rows="3"
-            value={notes}
-            onChange={(e) => setNotes(e.target.value)}
-          ></textarea>
-        </div>
+          <div class="mb-3">
+            <Form.Label htmlFor="milk-notes" className="register-form-label">
+              Notes
+            </Form.Label>
+            <textarea
+              class="form-control"
+              id="milk-notes"
+              rows="3"
+              value={notes}
+              onChange={(e) => setNotes(e.target.value)}
+            ></textarea>
+          </div>
+        </Form>
       </div>
     </>
   );
