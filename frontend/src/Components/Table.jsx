fe6d758e9d5dab33f8172e452d363badb7d01f2f
Parker <xparkerland@gmail.com>
2024-10-29 20:13:50 +1100

    Formated table so that can be altered depend on parent page

--- frontend/src/Components/Table.jsx
+++ frontend/src/Components/Table.jsx
@@ -2,137 +2,40 @@ import React from 'react'
 import Button from "react-bootstrap/Button";
 import { faPlus, faFilter, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export {Table};
 
-function Table({ setOpenModal }) {
-  let fake_data = [
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "DELETE"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "EDIT"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "ADD"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "DELETE"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "EDIT"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "ADD"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "DELETE"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "EDIT"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "ADD"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "DELETE"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "EDIT"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "ADD"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "DELETE"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "EDIT"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "ADD"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "DELETE"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "EDIT"      
-    },
-    {
-      mrn: 346363,
-      time: 1,
-      name: "Larkin",
-      type: "fresh",
-      action: "ADD"      
-    }
-  ]
+function Table({ data, setOpenModal, viewType }) {
+
+  const viewConfigs = {
+    "viewMilk": [
+      { label: "milk ID", key: "uid" },
+      { label: "Baby", key: "baby_mrn" },
+      { label: "Mother", key: "mother_mrn" },
+      { label: "Express time", key: "express_time" },
+      { label: "Expiration time", key: "expiration_time"},
+      { label: "Note", key: "extra_notes"},
+    ],
+    "viewMother": [
+      { label: "MRN", key: "mrn" },
+      { label: "First Name", key: "first_name" },
+      { label: "Last Name", key: "last_name"},
+      { label: "Associate Baby(ies)", key: "babies" }
+    ],
+    "viewBaby": [
+      { label: "MRN", key: "mrn" },
+      { label: "First Name", key: "first_name" },
+      { label: "Last Name", key: "last_name"},
+      { label: "Mother", key: "mother_mrn" }
+    ],
+    "viewLog": [
+      { label: "Log ID", key: "logId" },
+      { label: "Timestamp", key: "timestamp" },
+      { label: "Event", key: "event" }
+    ]
+  };
+  const columns = viewConfigs[viewType] || [];
+  
   return (
       <div className="table-container">
         <table>
@@ -142,38 +45,44 @@ function Table({ setOpenModal }) {
                 <div className="table-header">
                   <button className="btn btn-outline-secondary sort-btn"><FontAwesomeIcon icon={faFilter} /></button>
                   <input className="search-bar form-control" type="text" placeholder="Search..."/>
-                  <Button id="scan-btn" onClick={() => setOpenModal(true)}>
+                  {viewType === "viewMilk" && <Button id="scan-btn" onClick={() => setOpenModal(true)}>
                     <FontAwesomeIcon icon={faPlus} /> New Milk Entry
-                  </Button>
+                  </Button>}
                 </div>
               </td>
             </tr>
             <tr>
-              <th>#</th>
-              <th>Baby Name</th>
-              <th>type</th>
-              <th>Collection time</th>
-              <th>Expiry time</th>
-              <th colSpan={2}>Notes</th>
+            {columns.map((column) => (
+              column.key === "extra_notes" ? ( 
+                <th colSpan={2} key={column.key}>{column.label}</th>
+              ) : (
+                <th key={column.key}>{column.label}</th>
+              )
+            ))}
             </tr>
           </thead>
           
           <tbody>
-            {fake_data.length > 0 ? (
-              fake_data.map((item, index) => (
-                <tr key={item.mrn} className={index % 2 === 0 ? "even-row" : "odd-row"}>
-                  <th>{item.mrn}</th>
-                  <td>{item.name}</td>
-                  <td>{item.type}</td>
-                  <td>{item.time}</td>
-                  <td>{item.time}</td>
-                  <td>{item.action}</td>
-                  <td className="table-btn"><button className="dlt-btn"><FontAwesomeIcon icon={faTrash} /></button></td>
-                </tr>
-              ))
-            ) : (
+          {data.length > 0 ? (
+            data.map((item, index) => (
+              <tr key={item.uid || item.mrn || index} className={index % 2 === 0 ? "even-row" : "odd-row"}>
+                {columns.map((column) => (
+                  <td key={column.key}>
+                    {item[column.key]}
+                  </td>
+                ))}
+                {viewType === "viewMilk" && (
+                  <td key="delete-button">
+                    <button className="dlt-btn">
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </td>
+                )}
+              </tr>
+            ))
+          ) : (
               <tr>
-                <td colSpan="4">Loading...</td>
+                <td colSpan={columns.length}>Loading...</td>
               </tr>
             )}
           </tbody>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Navibar } from "../Components/Navibar";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "../index.css";
 import { AddMilkModal } from "../Components/AddMilkModal";
 import { Table } from "../Components/Table";
@@ -12,15 +9,42 @@ export { Home };
 
 function Home() {
   const [openModal, setOpenModal] = useState(false);
+  // call backend to get milk infos
 
+  let fake_data = [
+    {
+      uid: 346363,
+      express_time: 1,
+      expiration_time: 2,
+      baby_mrn: "43636436",
+      mother_mrn: "21412451251",
+      extra_notes: "im notes"      
+    },
+    {
+      uid: 346363,
+      express_time: 1,
+      expiration_time: 2,
+      baby_mrn: "43636436",
+      mother_mrn: "21412451251",
+      extra_notes: "im notes"      
+    },
+    {
+      uid: 346363,
+      express_time: 1,
+      expiration_time: 2,
+      baby_mrn: "43636436",
+      mother_mrn: "21412451251",
+      extra_notes: "im notes"      
+    }
+  ]
   return (
     <>
       <section id="Home">
         <Navibar />
         <div className="page-container">
           <h1 className="page-title">List of Milk Entries</h1>
-          <p>sub - info</p>
-          <Table setOpenModal = {setOpenModal} />
+          <p>Total Number of Milk Entries: {fake_data.length}</p>
+          <Table data={fake_data} setOpenModal = {setOpenModal} viewType="viewMilk"/>
         </div>
         {openModal && (
           <AddMilkModal closeModal={setOpenModal} version="addMilk1" />
