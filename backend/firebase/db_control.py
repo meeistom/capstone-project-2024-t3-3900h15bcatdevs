36a1046cb298705a04e267a7ae41da6b5f0eff56
Gabriel Thien <53280648+losgab@users.noreply.github.com>
2024-11-03 02:57:40 +1100

    Update db_control.py

--- backend/firebase/db_control.py
+++ backend/firebase/db_control.py
@@ -5,14 +5,9 @@ import json
 import firebase_admin as fba
 from firebase_admin import firestore, credentials
 
-from firebase.error_check import *
+from error_check import *
 
-# cred = credentials.Certificate('./firebase/key_gabe.json')
-# cred = credentials.Certificate('./firebase/key_cynthia.json')
-cred = credentials.Certificate('../firebase/key_aolin.json')
-# cred = credentials.Certificate('./firebase/key_parker.json')
-# cred = credentials.Certificate('./firebase/key_tom.json')
-# cred = credentials.Certificate('./firebase/key_parker.json')
+cred = credentials.Certificate('./.key/key2.json')
 
 fba.initialize_app(cred)
 fs_client = firestore.client()
@@ -37,10 +32,9 @@ def add_dummy_data(firestore_client, collection_name: str, json_path: str):
 
 # if __name__ == '__main__':
 #     clear_collection(fs_client, "mothers")
-#     print("cleared");
 #     clear_collection(fs_client, "babies")
 #     clear_collection(fs_client, "milk_entries")
 
-#     add_dummy_data(fs_client, "mothers", "../firebase/data/mother_details.json")
-#     add_dummy_data(fs_client, "babies", "../firebase/data/baby_details.json")
-#     add_dummy_data(fs_client, "milk_entries", "../firebase/data/bottle_details.json")
+#     add_dummy_data(fs_client, "mothers", "./firebase/data/mum_details_reduced.json")
+#     add_dummy_data(fs_client, "babies", "./firebase/data/baby_details_reduced.json")
+#     add_dummy_data(fs_client, "milk_entries", "./firebase/data/milk_details_reduced.json")
