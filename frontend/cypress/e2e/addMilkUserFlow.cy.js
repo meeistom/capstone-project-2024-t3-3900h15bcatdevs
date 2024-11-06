aa3f487fd5532236a3848925cda8cc3c6c59a807
skyuanow <skyuanow@gmail.com>
2024-11-06 21:25:31 +1100

    updated add milk flow test with label generation feature

--- frontend/cypress/e2e/addMilkUserFlow.cy.js
+++ frontend/cypress/e2e/addMilkUserFlow.cy.js
@@ -2,7 +2,10 @@ const url = "http://localhost:3000/";
 
 describe("user add milk bottle happy path", () => {
   beforeEach(() => {
+    cy.intercept("/home").as("home");
+    cy.intercept("/notifications").as("notifications");
     cy.visit(url);
+    cy.wait(["@home", "@notifications"]);
     cy.get("#scan-btn").click();
   });
 
@@ -63,7 +66,7 @@ describe("user add milk bottle happy path", () => {
 
     cy.get("button[name='preview-sticker']").click();
 
-    cy.get('img[alt="sticker"]').should("exist");
+    cy.get(".body").find("svg").should("exist");
   });
 
   it("should not submit form if not all fields are valid", () => {
@@ -74,7 +77,7 @@ describe("user add milk bottle happy path", () => {
 
     cy.get("button[name='preview-sticker']").click();
 
-    cy.get('img[alt="sticker"]').should("not.exist");
+    cy.get(".body").find("svg").should("not.exist");
   });
 
   it("should have a confirmation page of the milk submission", () => {
@@ -99,7 +102,7 @@ describe("user add milk bottle happy path", () => {
     cy.get("#expiry-date").should("have.value", expiryDate);
 
     cy.get("button[name='preview-sticker']").click();
-    cy.get('img[alt="sticker"]').should("exist");
+    cy.get(".body").find("svg").should("exist");
 
     cy.get("button[name='confirm-and-print']").click();
     cy.get('img[alt="Confirmation Icon"]').should("exist");
