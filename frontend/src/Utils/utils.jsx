c9ff783e34935c626ed4c72a77e11c83a1d36266
skyuanow <skyuanow@gmail.com>
2024-11-06 17:33:00 +1100

    implemented generate label for frontend Register

--- frontend/src/Utils/utils.jsx
+++ frontend/src/Utils/utils.jsx
@@ -4,9 +4,7 @@ export const toUnix = (dateTime) => {
 };
 
 export const dateTimeToString = (dateTime) => {
-  console.log(dateTime);
   const [date, time] = dateTime.split("T");
-  console.log(date, time);
   const [year, month, day] = date.split("-");
   return `${day}/${month}/${year} ${time}`;
 };
