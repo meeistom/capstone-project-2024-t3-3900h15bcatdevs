191e5895a7606f7ea4879fe488764af691515e6c
skyuanow <skyuanow@gmail.com>
2024-11-09 11:54:38 +1100

    implemented delete milk reason options in frontend

--- frontend/src/Components/DeleteMilkModal.jsx
+++ frontend/src/Components/DeleteMilkModal.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../index.css";
 
 export { DeleteMilkModal };
+
 function DeleteMilkModal({ closeModal, entry, deleteMilk }) {
+  const [reason, setReason] = useState("expired");
+  const [notes, setNotes] = useState("");
 
   return (
     <>
@@ -9,25 +17,85 @@ function DeleteMilkModal({ closeModal, entry, deleteMilk }) {
         id={`${entry.uid}-deletion-modal`}
         className="modal-background position-fixed d-flex justify-content-center align-items-center w-100 h-100"
       >
-        <div className="modal fade show" style={{ display: 'block' }} tabIndex="-1">
+        <div
+          className="modal fade show"
+          style={{ display: "block" }}
+          tabIndex="-1"
+        >
           <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title">Deletion Confirmation</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => closeModal(false)}></button>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                  onClick={() => closeModal(false)}
+                ></button>
               </div>
-              <div className="modal-body">
-                <p>Are you sure you want to delete this milk entry?</p>
-                Milk {entry.uid} belongs to: <br></br> {entry.baby_name}, baby of {entry.mother_name}
+              <div className="modal-body d-flex flex-column ">
+                <p>
+                  Milk #{entry.uid} belongs to: <br /> {entry.baby_name}, baby
+                  of {entry.mother_name}
+                </p>
+                <div className="removal-form">
+                  <Form.Label htmlFor="milk-storage" className="form-label">
+                    Reason for milk removal*
+                  </Form.Label>
+                  <Form.Select
+                    id="milk-removal-reason"
+                    className="form-select form-select-sm"
+                    defaultValue={reason}
+                    onChange={(e) => {
+                      setReason(e.target.value);
+                    }}
+                  >
+                    <option value="expired">Expired</option>
+                    <option value="discharged">Baby discharged</option>
+                    <option value="other">Other</option>
+                  </Form.Select>
+                  {reason === "other" && (
+                    <div className="removal-notes">
+                      <Form.Label
+                        htmlFor="removal-notes"
+                        className="form-label pt-2"
+                      >
+                        Notes*
+                      </Form.Label>
+                      <textarea
+                        className="form-control pt-2"
+                        id="removal-notes"
+                        rows="2"
+                        value={notes}
+                        onChange={(e) => {
+                          setNotes(e.target.value);
+                        }}
+                      ></textarea>
+                    </div>
+                  )}
+                </div>
               </div>
               <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => closeModal(false)}>Close</button>
-                <button type="button" className="btn btn-danger" onClick={() => deleteMilk(entry.uid)} >Delete</button>
+                <Button
+                  variant="secondary"
+                  data-bs-dismiss="modal"
+                  onClick={() => closeModal(false)}
+                >
+                  Close
+                </Button>
+                <Button
+                  variant="danger"
+                  disabled={reason === "other" && notes === ""}
+                  onClick={() => deleteMilk(entry.uid, reason, notes)}
+                >
+                  Delete
+                </Button>
               </div>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
