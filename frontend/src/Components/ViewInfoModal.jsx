4ec4e9fc5a6c2e25b8039a3a75071d36b8917854
Aolin Xu <aolinzamxu@gmail.com>
2024-11-11 12:59:26 +1100

    make milk entry fields editable

--- frontend/src/Components/ViewInfoModal.jsx
+++ frontend/src/Components/ViewInfoModal.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { ViewInfoForm } from './ViewInfoForm';
-import { Modal } from './Modal';
+import React, { useState, useEffect } from "react";
+import { ViewInfoForm } from "./ViewInfoForm";
+import { Modal } from "./Modal";
 import Button from "react-bootstrap/Button";
+import { URL } from "../constants";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../index.css";
@@ -9,40 +10,38 @@ import "../index.css";
 export { ViewInfoModal };
 
 function ViewInfoModal({ info, closeModal }) {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState(null);
-  const [footer, setFooter] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedInfo, setEditedInfo] = useState(info);
 
-  useEffect(() => {
-    setTitle(`Milk ID #${info.uid}`);
-    setBody(<ViewInfoForm info={info}/>);
-    setFooter(
-      <>
-      <div id="btn-group" className="info-btn-group">
-        <Button onClick={() => closeModal(false)} variant="outline-primary">
-          Close
-        </Button>
-        <Button
-          name="edit-milk"
-          variant="primary"
-        >
-          Edit
-        </Button>
-      </div>
-      </>
-    );
-  }, [])
-  
+  const handleSave = () => {
+    fetch(`${URL}/add_milk_entry`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        setIsEditing(false);
+        closeModal(true);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
-    <>
-      <Modal
-        id={info.uid || info.mrn}
-        title={title}
-        body={body}
-        footer={footer}
-        closeModal={closeModal}
-      />
-    </>
-  )
+    <Modal
+      id={info.uid || info.mrn}
+      title={`Milk ID #${info.uid}`}
+      body={<ViewInfoForm info={editedInfo} isEditing={isEditing} onChange={setEditedInfo} />}
+      footer={
+        <div id="btn-group" className="info-btn-group">
+          <Button onClick={() => closeModal(false)} variant="outline-primary">
+            Close
+          </Button>
+          <Button onClick={() => (isEditing ? handleSave() : setIsEditing(true))} variant="primary">
+            {isEditing ? "Save" : "Edit"}
+          </Button>
+        </div>
+      }
+      closeModal={closeModal}
+    />
+  );
 }
