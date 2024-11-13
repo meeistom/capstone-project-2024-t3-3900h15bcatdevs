33ce96749d013500f0c545e43a240a8597b9da7d
Parker <xparkerland@gmail.com>
2024-11-13 18:07:35 +1100

    updated unix to time str now no longer has coma in the middle

--- frontend/src/Utils/utils.jsx
+++ frontend/src/Utils/utils.jsx
@@ -11,8 +11,19 @@ export const dateTimeToString = (dateTime) => {
 
 export const unixToTimeStr = (unixTime) => {
   const date = new Date(unixTime * 1000);
-  return date.toLocaleString();
+  return date
+    .toLocaleString('en-BG', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    })
+    .replace(',', '');
 };
+
 export const getCurrentDateTime = () => {
   const date = new Date();
   date.setDate(date.getDate());
