115a71654a8375764ab188f4f681eee31bcd458a
skyuanow <skyuanow@gmail.com>
2024-11-18 21:06:35 +1100

    updated navbar test

--- frontend/cypress/e2e/navigateFlow.cy.js
+++ frontend/cypress/e2e/navigateFlow.cy.js
@@ -1,46 +1,44 @@
-const url = "http://localhost:3000/";
+const url = 'http://localhost:3000/';
 
-describe("navigate pages flow", () => {
+describe('navigate pages flow', () => {
   beforeEach(() => {
+    cy.intercept('/home').as('home');
+    cy.intercept('/notifications').as('notifications');
     cy.visit(url);
+    cy.wait(['@home', '@notifications']);
   });
 
-  it("should navigate to the home page sucessfully", () => {
-    cy.url().should("include", url);
+  it('should navigate to the home page sucessfully', () => {
+    cy.url().should('include', url);
   });
 
-  it("should navigate to the view mothers page successfully", () => {
+  it('should navigate to the view mothers page successfully', () => {
     cy.get('[data-testid="nav-mothers"]').click();
-    cy.url().should("include", "/view_mothers");
+    cy.url().should('include', '/view_mothers');
   });
 
-  it("should navigate to the view babies page successfully", () => {
-    cy.get('[data-testid="nav-milks"]').click();
-    cy.url().should("include", "/view_milks");
-  });
-
-  it("should navigate to the register page successfully", () => {
+  it('should navigate to the register page successfully', () => {
     cy.get('[data-testid="nav-register"]').click();
-    cy.url().should("include", "/register");
+    cy.url().should('include', '/register');
   });
 
-  it("should navigate to the history page successfully", () => {
+  it('should navigate to the history page successfully', () => {
     cy.get('[data-testid="nav-history"]').click();
-    cy.url().should("include", "/history_log");
+    cy.url().should('include', '/history_log');
   });
 
-  it("should navigate to the verify page successfully", () => {
+  it('should navigate to the verify page successfully', () => {
     cy.get('[data-testid="nav-verify"]').click();
-    cy.url().should("include", "/verify_feed");
+    cy.url().should('include', '/verify_feed');
   });
 
-  it("should navigate to the home page successfully from another page", () => {
+  it('should navigate to the home page successfully from another page', () => {
     cy.get('[data-testid="nav-verify"]').click();
     cy.get('[data-testid="nav-home"]').click();
-    cy.url().should("include", url);
+    cy.url().should('include', url);
 
-    cy.get('[data-testid="nav-babies"]').click();
+    cy.get('[data-testid="nav-mothers"]').click();
     cy.get('[data-testid="nav-mg"]').click();
-    cy.url().should("include", url);
+    cy.url().should('include', url);
   });
 });
