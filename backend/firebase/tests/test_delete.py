70f2d5401574bf2a9e52b5c3878472a77ea319b9
skyuanow <skyuanow@gmail.com>
2024-11-18 22:45:39 +1100

    fixed backend delete test

--- backend/firebase/tests/test_delete.py
+++ backend/firebase/tests/test_delete.py
@@ -4,7 +4,7 @@ import firebase_admin as fba
 from firebase_admin import firestore, credentials
 
 if not fba._apps:
-    cred = credentials.Certificate("../.key/key.json") # MIGHT CHANGE TO key2.json WHEN MERGING BUT PROBABLY NOT
+    cred = credentials.Certificate(".key/key.json") # MIGHT CHANGE TO key2.json WHEN MERGING BUT PROBABLY NOT
     fba.initialize_app(cred)
 
 fs_client = firestore.client()
@@ -17,8 +17,8 @@ def test_delete_document():
         fs_client,
         mother_mrn='2055',
         baby_mrn='1215',
-        milk_entry_uid='000000'
-    ) == (True, f"Successfully deleted document {['2055', '1215', '000000']}")
+        milk_entry_uid='m000'
+    ) == (True, f"Successfully deleted document {['2055', '1215', 'm000']}")
 
     assert delete_document(
         fs_client,
@@ -32,8 +32,8 @@ def test_delete_document():
 
     assert delete_document(
         fs_client,
-        milk_entry_uid='000001'
-    ) == (True, f"Successfully deleted document {['000001']}")
+        milk_entry_uid='m001'
+    ) == (True, f"Successfully deleted document {['m001']}")
 
     assert delete_document(
         fs_client,
@@ -44,14 +44,14 @@ def test_delete_document():
     assert delete_document(
         fs_client,
         mother_mrn='4487',
-        milk_entry_uid='000004' # was '000002' before but caused issue since mother was already deleted - REMOVE THIS COMMENT LATER
-    ) == (True, f"Successfully deleted document {['4487', '000004']}") # was '000002' before but caused issue since mother was already deleted - REMOVE THIS COMMENT LATER
+        milk_entry_uid='m004' # was '000002' before but caused issue since mother was already deleted - REMOVE THIS COMMENT LATER
+    ) == (True, f"Successfully deleted document {['4487', 'm004']}") # was '000002' before but caused issue since mother was already deleted - REMOVE THIS COMMENT LATER
 
     assert delete_document(
         fs_client,
         baby_mrn='4697',
-        milk_entry_uid='000003'
-    ) == (True, f"Successfully deleted document {['4697', '000003']}")
+        milk_entry_uid='m003'
+    ) == (True, f"Successfully deleted document {['4697', 'm003']}")
 
     # Test 2: No fields given
     assert delete_document(
@@ -63,17 +63,17 @@ def test_delete_document():
         fs_client,
         mother_mrn='2055',
         baby_mrn='1215',
-        milk_entry_uid='000000'
+        milk_entry_uid='m000'
     ) == (False, f"MRN/UID {'2055'} does not exist in collection {'mothers'}")
 
     assert delete_document(
         fs_client,
         baby_mrn='1215',
-        milk_entry_uid='000000'
+        milk_entry_uid='m000'
     ) == (False, f"MRN/UID {'1215'} does not exist in collection {'babies'}")
 
     assert delete_document(
         fs_client,
-        milk_entry_uid='000000'
-    ) == (False, f"MRN/UID {'000000'} does not exist in collection {'milk_entries'}")
+        milk_entry_uid='m000'
+    ) == (False, f"MRN/UID {'m000'} does not exist in collection {'milk_entries'}")
     
